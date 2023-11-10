b0a189577d2b017699883cf00c82f821f535b039
Jack He <jackhe@microsoft.com>
2023-11-09 18:25:08 -0800

    ready for github pages

---
+++ dist/assets/isMuiElement-85a2be06.js
@@ -0,0 +1,1 @@
+import{r as u}from"./index-43a5b071.js";function m(i,a){var l,e;return u.isValidElement(i)&&a.indexOf((l=i.type.muiName)!=null?l:(e=i.type)==null||(e=e._payload)==null||(e=e.value)==null?void 0:e.muiName)!==-1}export{m as i};
---
+++ dist/assets/usePreviousProps-96acc879.js
@@ -0,0 +1,1 @@
+import{r as e}from"./index-43a5b071.js";const o=s=>{const r=e.useRef({});return e.useEffect(()=>{r.current=s}),r.current},u=o;export{u};
