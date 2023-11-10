dd23c974569ff7e6a53bb9ccd06e3aae0a19bad0
Jack He <jackhe@microsoft.com>
2023-11-09 18:23:19 -0800

    add code for production dashboard

---
+++ dashboard/src/components/logo/logo.jsx
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types';
+import { forwardRef } from 'react';
+
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+// import { useTheme } from '@mui/material/styles';
+
+import { RouterLink } from 'src/routes/components';
+
+// ----------------------------------------------------------------------
+
+const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
+  // const theme = useTheme();
+
+  // const PRIMARY_LIGHT = theme.palette.primary.light;
+
+  // const PRIMARY_MAIN = theme.palette.primary.main;
+
+  // const PRIMARY_DARK = theme.palette.primary.dark;
+
+  // OR using local (public folder)
+  // -------------------------------------------------------
+  // const logo = (
+  //   <Box
+  //     component="img"
+  //     src="/logo/logo_single.svg" => your path
+  //     sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
+  //   />
+  // );
+
+
+
+  const logo = (
+    <Box
+      ref={ref}
+      component="div"
+      sx={{
+        width: 40,
+        height: 40,
+        display: 'inline-flex',
+        ...sx,
+      }}
+      {...other}
+    >
+      <br/>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23"><path fill="#f3f3f3" d="M0 0h23v23H0z" /><path fill="#f35325" d="M1 1h10v10H1z" /><path fill="#81bc06" d="M12 1h10v10H12z" /><path fill="#05a6f0" d="M1 12h10v10H1z" /><path fill="#ffba08" d="M12 12h10v10H12z" /></svg>
+    </Box>
+  );
+
+  if (disabledLink) {
+    return logo;
+  }
+
+  return (
+    <Link component={RouterLink} href="/" sx={{ display: 'contents' }}>
+      {logo}
+      <br/>
+      <br />
+    </Link>
+  );
+});
+
+Logo.propTypes = {
+  disabledLink: PropTypes.bool,
+  sx: PropTypes.object,
+};
+
+export default Logo;
