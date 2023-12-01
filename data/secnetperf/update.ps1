a6adaaf363e9dd73dd90b15aab5a30049da6a105
Jack He <jackhe@microsoft.com>
2023-12-01 11:51:37 -0800

    add script to update the dev bot

---
+++ data/secnetperf/update.ps1
@@ -0,0 +1,20 @@
+
+param(
+    [string]$ref = 'manual'
+)
+
+$Date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+
+$RunName = "$Date-$ref"
+
+$env:GIT_REDIRECT_STDERR = '2>&1'
+
+mv test_result.json "$RunName.json"
+mv "$RunName.json" ./data/secnetperf
+git config user.email "quicdev@microsoft.com"
+git config user.name "QUIC Dev[bot]"
+git add ./data
+git status
+git commit -m "Add test results for $RunName"
+git pull
+git push
