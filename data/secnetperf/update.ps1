2195a9734ad6e1783658cd9f5d5c4bd68a219579
Jack He <jackhe@microsoft.com>
2023-12-03 18:57:20 -0800

    modify update.ps1 script to only include date

--- data/secnetperf/update.ps1
+++ data/secnetperf/update.ps1
@@ -3,18 +3,33 @@ param(
     [string]$ref = 'manual'
 )
 
-$Date = Get-Date -Format "yyyy-MM-dd-HH-mm-ss"
+# Get the current date and time
+$currentDate = Get-Date
 
-$RunName = "$Date._.$ref"
+# Define the Pacific Standard Time zone ID
+# Note: Windows time zones might not use "PST" as the identifier.
+# Instead, it often uses specific city names or "Pacific Standard Time"
+$pstZoneId = "Pacific Standard Time"
+
+# Get the TimeZoneInfo object for Pacific Standard Time
+$pstZone = [TimeZoneInfo]::FindSystemTimeZoneById($pstZoneId)
+
+# Convert the current date and time to Pacific Standard Time
+$pstDate = [TimeZoneInfo]::ConvertTime($currentDate, $pstZone)
+
+# Format the date in the "yyyy-MM-dd-HH-mm-ss" format
+$formattedDate = $pstDate.ToString("yyyy-MM-dd-HH-mm-ss")
+
+$RunName = $formattedDate
 
 $env:GIT_REDIRECT_STDERR = '2>&1'
 
-mv test_result.json "$RunName.json"
-mv "$RunName.json" ./data/secnetperf
+mv test_result.json $RunName
+mv $RunName ./data/secnetperf
 git config user.email "quicdev@microsoft.com"
 git config user.name "QUIC Dev[bot]"
 git add ./data
 git status
-git commit -m "Add test results for $RunName"
+git commit -m "Add test results for $ref"
 git pull
 git push
