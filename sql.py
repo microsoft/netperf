839a38b4fe6ee347b4831983eec90255eb3c46ac
Jack He <jackhe@microsoft.com>
2023-12-08 13:22:00 -0800

    can execute .sql files with python and add them to DB

---
+++ sql.py
@@ -0,0 +1,20 @@
+import sqlite3
+import glob
+
+# Connect to the database
+conn = sqlite3.connect('netperf.sqlite')
+cursor = conn.cursor()
+
+# Iterate over all .sql files in the current directory
+for sql_file_path in glob.glob('*.sql'):
+    with open(sql_file_path, 'r') as sql_file:
+        sql_script = sql_file.read()
+
+        # Execute SQL script
+        cursor.executescript(sql_script)
+
+        # Commit changes
+        conn.commit()
+
+# Close connection
+conn.close()
