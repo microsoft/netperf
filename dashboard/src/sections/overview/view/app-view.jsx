dcf02a4ade5d456afc5feb6543299d57a3caf434
Jack He (Github) <jackhe2345@gmail.com>
2024-01-05 06:00:34 -0800

    cleanup (#26)

--- dashboard/src/sections/overview/view/app-view.jsx
+++ dashboard/src/sections/overview/view/app-view.jsx
@@ -1,122 +1,121 @@
 /* eslint-disable no-restricted-syntax */
 
-// import { faker } from '@faker-js/faker';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
-// import Iconify from 'src/components/iconify';
-
 import useFetchData from 'src/hooks/use-fetch-data';
-// import AppTasks from '../app-tasks';
-// import AppNewsUpdate from '../app-news-update';
-// import AppOrderTimeline from '../app-order-timeline';
-// import AppCurrentVisits from '../app-current-visits';
 import AppWebsiteVisits from '../app-website-visits';
 import AppWidgetSummary from '../app-widget-summary';
-// import AppTrafficBySite from '../app-traffic-by-site';
-// import AppCurrentSubject from '../app-current-subject';
-// import AppConversionRates from '../app-conversion-rates';
-
-
 
 function throughputPerformance(download, upload, dweight, uweight) {
-  return ((download * dweight) + (upload * uweight)) / 10000;
+  return (download * dweight + upload * uweight) / 10000;
 }
 
 function latencyPerformance(latencies) {
-  const weighting = [0.05, 0.1, 0.2, 0.3, 0.1, 0.1, 0.1, 0.05]
-  let sum = 1.0
+  const weighting = [0.05, 0.1, 0.2, 0.3, 0.1, 0.1, 0.1, 0.05];
+  let sum = 1.0;
   for (let i = 0; i < latencies.length; i += 1) {
-    sum += weighting[i] * latencies[i]
+    sum += weighting[i] * latencies[i];
   }
-  return (1 / sum) * 100000
+  return (1 / sum) * 100000;
 }
-
 
 // ----------------------------------------------------------------------
 
 export default function AppView() {
-  const windows = useFetchData("https://raw.githubusercontent.com/microsoft/netperf/deploy/json-test-results-windows-windows-2022-x64-schannel.json");
-  const linux = useFetchData("https://raw.githubusercontent.com/microsoft/netperf/deploy/json-test-results-linux-ubuntu-20.04-x64-openssl.json");
+  const windows = useFetchData(
+    'https://raw.githubusercontent.com/microsoft/netperf/deploy/json-test-results-windows-windows-2022-x64-schannel.json'
+  );
+  const linux = useFetchData(
+    'https://raw.githubusercontent.com/microsoft/netperf/deploy/json-test-results-linux-ubuntu-20.04-x64-openssl.json'
+  );
 
+  let windowsPerfScore = 0;
+  let linuxPerfScore = 0;
 
-  let windowsPerfScore = 0
-  let linuxPerfScore = 0
+  let windowsPerfScoreLatency = 0;
+  let linuxPerfScoreLatency = 0;
 
-  let windowsPerfScoreLatency = 0
-  let linuxPerfScoreLatency = 0
+  let windowsUploadThroughputQuic = 1;
+  let windowsUploadThroughputTcp = 1;
+  let windowsDownloadThroughputQuic = 1;
+  let windowsDownloadThroughputTcp = 1;
 
-  let windowsUploadThroughputQuic = 1
-  let windowsUploadThroughputTcp = 1
-  let windowsDownloadThroughputQuic = 1
-  let windowsDownloadThroughputTcp = 1
+  let linuxDownloadThroughputQuic = 1;
+  let linuxDownloadThroughputTcp = 1;
+  let linuxUploadThroughputQuic = 1;
+  let linuxUploadThroughputTcp = 1;
 
-  let linuxDownloadThroughputQuic = 1
-  let linuxDownloadThroughputTcp = 1
-  let linuxUploadThroughputQuic = 1
-  let linuxUploadThroughputTcp = 1
+  let windowsLatencyQuic = [0, 0, 0, 0, 0, 0, 0, 0];
+  let windowsLatencyTcp = [0, 0, 0, 0, 0, 0, 0, 0];
+  let linuxLatencyQuic = [0, 0, 0, 0, 0, 0, 0, 0];
+  let linuxLatencyTcp = [0, 0, 0, 0, 0, 0, 0, 0];
 
-  let windowsLatencyQuic = [0, 0, 0, 0, 0, 0, 0, 0]
-  let windowsLatencyTcp = [0, 0, 0, 0, 0, 0, 0, 0]
-  let linuxLatencyQuic = [0, 0, 0, 0, 0, 0, 0, 0]
-  let linuxLatencyTcp = [0, 0, 0, 0, 0, 0, 0, 0]
-
-  const windowsType = "Windows Server 2022";
-  const linuxType = "Linux Ubuntu 20.04 LTS"
+  const windowsType = 'Windows Server 2022';
+  const linuxType = 'Linux Ubuntu 20.04 LTS';
 
   if (windows.data && linux.data) {
     for (const key of Object.keys(windows.data)) {
-      if (key.includes("download") && key.includes("quic")) {
+      if (key.includes('download') && key.includes('quic')) {
         windowsDownloadThroughputQuic = windows.data[key];
       }
-      if (key.includes("download") && key.includes("tcp")) {
+      if (key.includes('download') && key.includes('tcp')) {
         windowsDownloadThroughputTcp = windows.data[key];
       }
-      if (key.includes("upload") && key.includes("quic")) {
+      if (key.includes('upload') && key.includes('quic')) {
         windowsUploadThroughputQuic = windows.data[key];
       }
-      if (key.includes("upload") && key.includes("tcp")) {
+      if (key.includes('upload') && key.includes('tcp')) {
         windowsUploadThroughputTcp = windows.data[key];
       }
-      if (key.includes("rps") && key.includes("quic")) {
+      if (key.includes('rps') && key.includes('quic')) {
         windowsLatencyQuic = windows.data[key];
       }
-      if (key.includes("rps") && key.includes("tcp")) {
+      if (key.includes('rps') && key.includes('tcp')) {
         windowsLatencyTcp = windows.data[key];
       }
     }
 
     for (const key of Object.keys(linux.data)) {
-      if (key.includes("download") && key.includes("quic")) {
+      if (key.includes('download') && key.includes('quic')) {
         linuxDownloadThroughputQuic = linux.data[key];
       }
-      if (key.includes("download") && key.includes("tcp")) {
+      if (key.includes('download') && key.includes('tcp')) {
         linuxDownloadThroughputTcp = linux.data[key];
       }
-      if (key.includes("upload") && key.includes("quic")) {
+      if (key.includes('upload') && key.includes('quic')) {
         linuxUploadThroughputQuic = linux.data[key];
       }
-      if (key.includes("upload") && key.includes("tcp")) {
+      if (key.includes('upload') && key.includes('tcp')) {
         linuxUploadThroughputTcp = linux.data[key];
       }
-      if (key.includes("rps") && key.includes("quic")) {
+      if (key.includes('rps') && key.includes('quic')) {
         linuxLatencyQuic = linux.data[key];
       }
-      if (key.includes("rps") && key.includes("tcp")) {
+      if (key.includes('rps') && key.includes('tcp')) {
         linuxLatencyTcp = linux.data[key];
       }
     }
 
-    windowsPerfScore = throughputPerformance(windowsDownloadThroughputQuic, windowsUploadThroughputQuic, 0.8, 0.2);
-    linuxPerfScore = throughputPerformance(linuxDownloadThroughputQuic, linuxUploadThroughputQuic, 0.8, 0.2);
-    
+    windowsPerfScore = throughputPerformance(
+      windowsDownloadThroughputQuic,
+      windowsUploadThroughputQuic,
+      0.8,
+      0.2
+    );
+    linuxPerfScore = throughputPerformance(
+      linuxDownloadThroughputQuic,
+      linuxUploadThroughputQuic,
+      0.8,
+      0.2
+    );
+
     windowsPerfScoreLatency = latencyPerformance(windowsLatencyQuic);
     linuxPerfScoreLatency = latencyPerformance(linuxLatencyQuic);
-    console.log(windowsPerfScoreLatency)
-    console.log(linuxPerfScoreLatency)
+    console.log(windowsPerfScoreLatency);
+    console.log(linuxPerfScoreLatency);
   }
-
 
   // LATENCY ARRAY: [0th, 50th, 90th, 99th, 99.9th, 99.99th, 99.999th, 99.9999th]
   // console.log(linuxLatencyQuic)
@@ -129,40 +128,7 @@ export default function AppView() {
       <Typography variant="h3" sx={{ mb: 5 }}>
         Network Performance Overview
       </Typography>
-
-      {/* <p>Data as of 10/10/2023 (Latest Commit)</p> */}
-      {/* <Typography variant="h6" sx={{ mb: 5 }}>
-        Network Performance Overview
-      </Typography> */}
-
       <Grid container spacing={3}>
-        {/* <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Weekly Sales"
-            total={714000}
-            color="success"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="New Users"
-            total={1352831}
-            color="info"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Item Orders"
-            total={1723315}
-            color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
-          />
-        </Grid> */}
-
         <Grid xs={12} sm={6} md={3}>
           <AppWidgetSummary
             title="Windows Throughput Performance Score."
@@ -203,8 +169,8 @@ export default function AppView() {
               <div>
                 <img alt="icon" src="/netperf/dist/assets/icons/glass/Ubuntu-Logo.png" />
                 <Button
-                onClick={() =>
-                  alert(`
+                  onClick={() =>
+                    alert(`
                 This score is computed as:
 
                 LINUX = download_speed * download_weight + upload_speed * upload_weight
@@ -217,8 +183,10 @@ export default function AppView() {
                 are using download a lot more often than upload.
 
             `)
-                }
-                >?</Button>
+                  }
+                >
+                  ?
+                </Button>
               </div>
             }
           />
@@ -265,8 +233,8 @@ export default function AppView() {
               <div>
                 <img alt="icon" src="/netperf/dist/assets/icons/glass/Ubuntu-Logo.png" />
                 <Button
-                onClick={() =>
-                  alert(`
+                  onClick={() =>
+                    alert(`
               This score is computed as:
               
               This score is computed as:
@@ -283,8 +251,10 @@ export default function AppView() {
               Therefore, we give less weighting to the perfect case (0th percentile). 
 
             `)
-                }
-                >?</Button>
+                  }
+                >
+                  ?
+                </Button>
               </div>
             }
           />
@@ -301,211 +271,94 @@ export default function AppView() {
                   name: 'TCP',
                   type: 'column',
                   fill: 'solid',
-                  data: [windowsDownloadThroughputTcp, windowsUploadThroughputTcp, linuxDownloadThroughputTcp, linuxUploadThroughputTcp],
+                  data: [
+                    windowsDownloadThroughputTcp,
+                    windowsUploadThroughputTcp,
+                    linuxDownloadThroughputTcp,
+                    linuxUploadThroughputTcp,
+                  ],
                 },
                 {
                   name: 'QUIC',
                   type: 'column',
                   fill: 'solid',
-                  data: [windowsDownloadThroughputQuic, windowsUploadThroughputQuic, linuxDownloadThroughputQuic, linuxUploadThroughputQuic],
+                  data: [
+                    windowsDownloadThroughputQuic,
+                    windowsUploadThroughputQuic,
+                    linuxDownloadThroughputQuic,
+                    linuxUploadThroughputQuic,
+                  ],
                 },
               ],
             }}
           />
         </Grid>
-        {/*
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AppWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />}
-          />
-        </Grid> */}
 
         <Grid xs={12} md={6} lg={6}>
           <AppWebsiteVisits
             title="Latency Comparison (ms), lower the better."
-            subheader="Tested using Windows 11 build 22000.282, Linux Ubuntu 20.04.3 LTS"
+            subheader={`Tested using ${windowsType}, ${linuxType}`}
             chart={{
-              labels: ['Windows QUIC', 'Windows TCP', 'Linux QUIC', 'Linux TCP'],
+              // New labels based on percentiles
+              labels: [
+                '50th percentile',
+                '90th percentile',
+                '99th percentile',
+                '99.99th percentile',
+              ],
               series: [
                 {
-                  name: '50th percentile',
+                  name: 'Windows QUIC',
                   type: 'column',
                   fill: 'solid',
-                  data: [windowsLatencyQuic[1], windowsLatencyTcp[1], linuxLatencyQuic[1], linuxLatencyTcp[1]],
+                  // Data based on Windows QUIC for each percentile
+                  data: [
+                    windowsLatencyQuic[1],
+                    windowsLatencyQuic[2],
+                    windowsLatencyQuic[3],
+                    windowsLatencyQuic[4],
+                  ],
                 },
                 {
-                  name: '90th percentile',
+                  name: 'Windows TCP',
                   type: 'column',
                   fill: 'solid',
-                  data: [windowsLatencyQuic[2], windowsLatencyTcp[2], linuxLatencyQuic[2], linuxLatencyTcp[2]],
+                  // Data based on Windows TCP for each percentile
+                  data: [
+                    windowsLatencyTcp[1],
+                    windowsLatencyTcp[2],
+                    windowsLatencyTcp[3],
+                    windowsLatencyTcp[4],
+                  ],
                 },
                 {
-                  name: '99th percentile',
+                  name: 'Linux QUIC',
                   type: 'column',
                   fill: 'solid',
-                  data: [windowsLatencyQuic[3], windowsLatencyTcp[3], linuxLatencyQuic[3], linuxLatencyTcp[3]],
+                  // Data based on Linux QUIC for each percentile
+                  data: [
+                    linuxLatencyQuic[1],
+                    linuxLatencyQuic[2],
+                    linuxLatencyQuic[3],
+                    linuxLatencyQuic[4],
+                  ],
                 },
                 {
-                  name: '99.99th percentile',
+                  name: 'Linux TCP',
                   type: 'column',
                   fill: 'solid',
-                  data: [windowsLatencyQuic[4], windowsLatencyTcp[4], linuxLatencyQuic[4], linuxLatencyTcp[4]],
+                  // Data based on Linux TCP for each percentile
+                  data: [
+                    linuxLatencyTcp[1],
+                    linuxLatencyTcp[2],
+                    linuxLatencyTcp[3],
+                    linuxLatencyTcp[4],
+                  ],
                 },
               ],
             }}
           />
         </Grid>
-
-        {/* <Grid xs={12} md={6} lg={4}>
-          <AppCurrentVisits
-            title="Current Visits"
-            chart={{
-              series: [
-                { label: 'America', value: 4344 },
-                { label: 'Asia', value: 5435 },
-                { label: 'Europe', value: 1443 },
-                { label: 'Africa', value: 4443 },
-              ],
-            }}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={8}>
-          <AppConversionRates
-            title="Conversion Rates"
-            subheader="(+43%) than last year"
-            chart={{
-              series: [
-                { label: 'Italy', value: 400 },
-                { label: 'Japan', value: 430 },
-                { label: 'China', value: 448 },
-                { label: 'Canada', value: 470 },
-                { label: 'France', value: 540 },
-                { label: 'Germany', value: 580 },
-                { label: 'South Korea', value: 690 },
-                { label: 'Netherlands', value: 1100 },
-                { label: 'United States', value: 1200 },
-                { label: 'United Kingdom', value: 1380 },
-              ],
-            }}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={4}>
-          <AppCurrentSubject
-            title="Current Subject"
-            chart={{
-              categories: ['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math'],
-              series: [
-                { name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-                { name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-                { name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-              ],
-            }}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={8}>
-          <AppNewsUpdate
-            title="News Update"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: faker.person.jobTitle(),
-              description: faker.commerce.productDescription(),
-              image: `/assets/images/covers/cover_${index + 1}.jpg`,
-              postedAt: faker.date.recent(),
-            }))}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={4}>
-          <AppOrderTimeline
-            title="Order Timeline"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                '1983, orders, $4220',
-                '12 Invoices have been paid',
-                'Order #37745 from September',
-                'New order placed #XF-2356',
-                'New order placed #XF-2346',
-              ][index],
-              type: `order${index + 1}`,
-              time: faker.date.past(),
-            }))}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
-            title="Traffic by Site"
-            list={[
-              {
-                name: 'FaceBook',
-                value: 323234,
-                icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
-              },
-              {
-                name: 'Google',
-                value: 341212,
-                icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
-              },
-              {
-                name: 'Linkedin',
-                value: 411213,
-                icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
-              },
-              {
-                name: 'Twitter',
-                value: 443232,
-                icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
-              },
-            ]}
-          />
-        </Grid> */}
-
-        {/* <Grid xs={12} md={6} lg={8}>
-          <AppTasks
-            title="Tasks"
-            list={[
-              { id: '1', name: 'Create FireStone Logo' },
-              { id: '2', name: 'Add SCSS and JS files if required' },
-              { id: '3', name: 'Stakeholder Meeting' },
-              { id: '4', name: 'Scoping & Estimations' },
-              { id: '5', name: 'Sprint Showcase' },
-            ]}
-          />
-        </Grid> */}
       </Grid>
     </Container>
   );
