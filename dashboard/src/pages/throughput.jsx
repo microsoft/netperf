17511b9b1f1cc22df2175a21a090b19b5526af4e
Jack He <jackhe@microsoft.com>
2024-03-04 09:01:06 -0800

    update throughput page to be production ready

--- dashboard/src/pages/throughput.jsx
+++ dashboard/src/pages/throughput.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable */
 
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Grid from '@mui/material/Unstable_Grid2';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import useFetchData from 'src/hooks/use-fetch-data';
 import { GraphView } from 'src/sections/overview/graphing';
+
+
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 let isMouseDown = false;
 
@@ -21,120 +29,109 @@ document.addEventListener('mouseup', function() {
 
 export default function ThroughputPage() {
 
-  const URL = "https://raw.githubusercontent.com/microsoft/netperf/deploy/detailed_throughput_page.json";
+  const URL = "https://raw.githubusercontent.com/microsoft/netperf/deploy/historical_throughput_page.json";
   const { data } = useFetchData(URL);
   let uploadThroughput = <div />
   let downloadThroughput = <div />
 
+  const [env, setEnv] = useState('azure');
+
+  const [windowsOs, setWindowsOs] = useState('windows-2022-x64')
+
+  const [linuxOs, setLinuxOs] = useState('ubuntu-20.04-x64')
+
+  const [testType, setTestType] = useState('up')
+
   if (data) {
-    const indices = []
-    for (let i = 0; i < data["linuxTcpUploadThroughput"].length; i++) {
-      indices.push(i + 1)
-    }
-    data["linuxTcpUploadThroughput"].reverse();
-    data["windowsTcpUploadThroughput"].reverse();
-    data["linuxQuicUploadThroughput"].reverse();
-    data["windowsQuicUploadThroughput"].reverse();
-    data["linuxTcpDownloadThroughput"].reverse();
-    data["windowsTcpDownloadThroughput"].reverse();
-    data["linuxQuicDownloadThroughput"].reverse();
-    data["windowsQuicDownloadThroughput"].reverse();
+    // TODO: Should we find the max of windows / linux run and use that as our baseline?
+    let rep = data[`${windowsOs}-${env}-iocp-schannel`][`tput-${testType}-tcp`]['data'].slice().reverse();
+    let linuxRep = data[`${linuxOs}-${env}-epoll-openssl`][`tput-${testType}-tcp`]['data'].slice().reverse();
+    let indices = Array.from({length: Math.max(rep.length, linuxRep.length)}, (_, i) => i);
+
+    const tcpiocp = data[`${windowsOs}-${env}-iocp-schannel`][`tput-${testType}-tcp`]['data'].slice().reverse();
+    const quiciocp = data[`${windowsOs}-${env}-iocp-schannel`][`tput-${testType}-quic`]['data'].slice().reverse();
+    const tcpepoll = data[`${linuxOs}-${env}-epoll-openssl`][`tput-${testType}-tcp`]['data'].slice().reverse();
+    const quicepoll = data[`${linuxOs}-${env}-epoll-openssl`][`tput-${testType}-quic`]['data'].slice().reverse();
+    const quicxdp = data[`${windowsOs}-${env}-xdp-schannel`][`tput-${testType}-quic`]['data'].slice().reverse();
+    const quicwsk = data[`${windowsOs}-${env}-wsk-schannel`][`tput-${testType}-quic`]['data'].slice().reverse();
+
     uploadThroughput =
-      <GraphView title="Upload Throughput"
-    subheader='Tested using Windows Server 2022 (Client and Server). Linux Ubuntu 20.04.3 LTS (Client and Server). WIP, NOTE: each datapoint is the max of 3 runs.'
+      <GraphView title={`${testType === 'up' ? 'Upload' : 'Download'} Throughput`}
+    subheader={`Tested using ${windowsOs}, ${linuxOs}, taking the max of 3 runs. Test run args: `}
     labels={indices}
     map={(index) => {
       if (isMouseDown) {
-        window.location.href = `https://github.com/microsoft/msquic/commit/${data["linuxTcpUploadThroughput"][index][2]}`
+        window.location.href = `https://github.com/microsoft/msquic/commit/${rep[index][1]}`
       }
       return `<div style = "margin: 10px">
 
-          NOTE: still a WIP, data is the max of 3 runs.
+         <p> <b> Build date: </b> ${rep[index][3]} </p>
+         <p> <b> Specific Windows OS version this test ran on: </b> ${rep[index][2]} </p>
+         <p> <b> Specific Linux OS version this test ran on: </b> ${linuxRep[index][2]} </p>
+         <p> <b> Commit hash: </b> <a href="google.com"> ${rep[index][1]} </a> </p>
 
-         <p> <b> Build date: </b> ${data["linuxTcpUploadThroughput"][index][1]} </p>
-         <p> <b> Commit hash: </b> <a href="google.com"> ${data["linuxTcpUploadThroughput"][index][2]} </a> </p>
-
-         <p> <b> Linux TCP: </b> ${data["linuxTcpUploadThroughput"][index][0]} </p>
-         <p> <b> Windows TCP: </b> ${data["windowsTcpUploadThroughput"][index][0]} </p>
-         <p> <b> Linux QUIC: </b> ${data["linuxQuicUploadThroughput"][index][0]} </p>
-         <p> <b> Windows QUIC: </b> ${data["windowsQuicUploadThroughput"][index][0]} </p>
-
+         <p> <b> TCP + iocp: </b> ${tcpiocp[index] && tcpiocp[index][0]},
+         <p> <b> QUIC + iocp: </b> ${quiciocp[index] && quiciocp[index][0]}
+         <p> <b> TCP + epoll: </b> ${tcpepoll[index] && tcpepoll[index][0]}
+         <p> <b> QUIC + epoll: </b> ${quicepoll[index] && quicepoll[index][0]}
+         <p> <b> QUIC + winXDP: </b> ${quicxdp[index] && quicxdp[index][0]}
+         <p> <b> QUIC + wsk: </b> ${quicwsk[index] && quicwsk[index][0]}
       </div>`
     }}
     series={[
       {
-        name: 'Linux + TCP',
+        name: 'TCP + iocp',
         type: 'line',
         fill: 'solid',
-        data: data["linuxTcpUploadThroughput"].map((x) => x[0])
+        data: tcpiocp.map((x) => x[0]),
       },
       {
-        name: 'Windows + TCP',
+        name: 'QUIC + iocp',
         type: 'line',
         fill: 'solid',
-        data: data["windowsTcpUploadThroughput"].map((x) => x[0]),
+        data: quiciocp,
       },
       {
-        name: 'Linux + QUIC',
+        name: 'TCP + epoll',
         type: 'line',
         fill: 'solid',
-        data: data["linuxQuicUploadThroughput"].map((x) => x[0]),
+        data: tcpepoll,
       },
       {
-        name: 'Windows + QUIC',
+        name: 'QUIC + epoll',
         type: 'line',
         fill: 'solid',
-        data: data["windowsQuicUploadThroughput"].map((x) => x[0]),
+        data: quicepoll,
+      },
+      {
+        name: 'QUIC + winXDP',
+        type: 'line',
+        fill: 'solid',
+        data: quicxdp,
+      },
+      {
+        name: 'QUIC + wsk',
+        type: 'line',
+        fill: 'solid',
+        data: quicwsk,
       },
     ]} />
+  }
 
-    downloadThroughput =  <GraphView title="Download Throughput"
-    subheader='Tested using Windows Server 2022 (Client and Server). Linux Ubuntu 20.04.3 LTS (Client and Server). WIP, NOTE: each datapoint is the max of 3 runs.'
-    labels={indices}
-    map={(index) => {
-      if (isMouseDown) {
-        window.location.href = `https://github.com/microsoft/msquic/commit/${data["linuxTcpDownloadThroughput"][index][2]}`
-      }
-      return `<div style = "margin: 10px">
+  const handleChange = (event) => {
+    setEnv(event.target.value);
+  };
 
-          NOTE: still a WIP, data is the max of 3 runs.
+  const handleChangeWindowsOs = (event) => {
+    setWindowsOs(event.target.value);
+  }
 
-         <p> <b> Build date: </b> ${data["linuxTcpDownloadThroughput"][index][1]} </p>
-         <p> <b> Commit hash: </b> <a href="google.com"> ${data["linuxTcpDownloadThroughput"][index][2]} </a> </p>
+  const handleChangeLinuxOs = (event) => {
+    setLinuxOs(event.target.value);
+  }
 
-         <p> <b> Linux TCP: </b> ${data["linuxTcpDownloadThroughput"][index][0]} </p>
-         <p> <b> Windows TCP: </b> ${data["windowsTcpDownloadThroughput"][index][0]} </p>
-         <p> <b> Linux QUIC: </b> ${data["linuxQuicDownloadThroughput"][index][0]} </p>
-         <p> <b> Windows QUIC: </b> ${data["windowsQuicDownloadThroughput"][index][0]} </p>
-
-      </div>`
-    }}
-    series={[
-      {
-        name: 'Linux + TCP',
-        type: 'line',
-        fill: 'solid',
-        data: data["linuxTcpDownloadThroughput"].map((x) => x[0])
-      },
-      {
-        name: 'Windows + TCP',
-        type: 'line',
-        fill: 'solid',
-        data: data["windowsTcpDownloadThroughput"].map((x) => x[0]),
-      },
-      {
-        name: 'Linux + QUIC',
-        type: 'line',
-        fill: 'solid',
-        data: data["linuxQuicDownloadThroughput"].map((x) => x[0]),
-      },
-      {
-        name: 'Windows + QUIC',
-        type: 'line',
-        fill: 'solid',
-        data: data["windowsQuicDownloadThroughput"].map((x) => x[0]),
-      },
-    ]} />
+  const handleChangeTestType = (event) => {
+    setTestType(event.target.value);
   }
 
   return (
@@ -147,9 +144,77 @@ export default function ThroughputPage() {
         <Typography variant="h3" sx={{ mb: 5 }}>
           Detailed Throughput
         </Typography>
+        <div style={{display: 'flex'}}>
+        <Box sx={{ }}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Context</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={env}
+              label="Context"
+              onChange={handleChange}
+              defaultValue={0}
+            >
+              <MenuItem value='azure'>azure</MenuItem>
+              { windowsOs !== 'windows-2025-x64' && <MenuItem value='lab'>lab</MenuItem> }
+            </Select>
+          </FormControl>
+        </Box>
+        {/* <br /> */}
+        <Box sx={{ minWidth: 120, marginLeft: '10px' }}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Windows Environment</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={windowsOs}
+              label="Windows Environment"
+              onChange={handleChangeWindowsOs}
+              defaultValue={0}
+            >
+              <MenuItem value='windows-2022-x64'>windows-2022-x64</MenuItem>
+              { env === 'azure' && <MenuItem value='windows-2025-x64'>windows-2025-x64</MenuItem> }
+            </Select>
+          </FormControl>
+        </Box>
+        {/* <br /> */}
+        <Box sx={{ minWidth: 120, marginLeft: '10px' }}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Linux Environment</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={linuxOs}
+              label="Linux Environment"
+              onChange={handleChangeLinuxOs}
+              defaultValue={0}
+            >
+              <MenuItem value='ubuntu-20.04-x64'>ubuntu-20.04-x64</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Box sx={{ minWidth: 120, marginLeft: '10px' }}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Test type</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={testType}
+              label="Upload or download"
+              onChange={handleChangeTestType}
+              defaultValue={0}
+            >
+              <MenuItem value={'up'}>Upload</MenuItem>
+              <MenuItem value={'down'}>Download</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        </div>
+        <br />
+
         <Grid container spacing={3}>
           {uploadThroughput}
-          {downloadThroughput}
         </Grid>
       </Container>
     </>
