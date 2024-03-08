52948020c719858248f3ea5b87c97f7a55a45384
Nick Banks <nibanks@microsoft.com>
2024-03-08 09:03:58 -0500

    Create Azure VMs

---
+++ .github/workflows/create-azure-machines.ps1
@@ -0,0 +1,197 @@
+<#
+.SYNOPSIS
+    Creates a pair of Azure VMs to use for testing.
+#>
+
+param (
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("windows-2022", "windows-2025", "ubuntu-22.04", "ubuntu-20.04", "ubuntu-18.04", "mariner-2")]
+    [string]$Os = "windows-2022",
+
+    [Parameter(Mandatory = $false)]
+    [ValidateSet("Experimental_Boost4", "Standard_DS2_v2", "Standard_F8s_v2")]
+    [string]$VMSize = "Experimental_Boost4",
+
+    [Parameter(Mandatory = $false)]
+    [string]$ResourceGroupName = "netperf-ex",
+
+    [Parameter(Mandatory = $false)]
+    [string]$Location = "South Central US",
+
+    [Parameter(Mandatory = $false)]
+    [string]$SubscriptionId,
+
+    [Parameter(Mandatory = $false)]
+    [string]$GitHubToken,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Password
+)
+
+Set-StrictMode -Version "Latest"
+$PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+
+$osType = $Os.Split("-")[0]
+$image = "MicrosoftWindowsServer:WindowsServer:2022-datacenter-g2:latest"
+if ($Os -eq "windows-2025") {
+    Write-Error "Windows 2025 is not supported yet." # TODO - Get this working
+} elseif ($Os -eq "ubuntu-22.04") {
+    $image = "Canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:22.04.202403010"
+} elseif ($Os -eq "ubuntu-20.04") {
+    $image = "Canonical:0001-com-ubuntu-server-focal:20_04-lts-gen2:20.04.202402290"
+} elseif ($Os -eq "ubuntu-18.04") {
+    $image = "Canonical:UbuntuServer:18_04-lts-gen2:18.04.202402250"
+} elseif ($Os -eq "mariner-2") {
+    $image = "MicrosoftCBLMariner:cbl-mariner:cbl-mariner-2-gen2:2.20240223.01" # This image may not exist
+}
+$username = "secnetperf"
+$password = $Password | ConvertTo-SecureString -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($username, $password)
+
+if ($SubscriptionId) {
+    # Connect to Azure, otherwise assume we're already connected.
+    Connect-AzAccount -SubscriptionId $subscriptionId
+}
+
+Write-Host "Creating Azure Resources ($ResourceGroupName, $Location, $os, $VMSize)"
+
+try {
+    Get-AzResourceGroup -Name $ResourceGroupName | Out-Null
+    Write-Host "Found resource group"
+} catch {
+    New-AzResourceGroup -Name $ResourceGroupName -Location $Location | Out-Null
+    Write-Host "Created resource group"
+}
+
+$vnetName = "exvnet"
+try {
+    $vnet = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnetName
+    Write-Host "Found vnet"
+} catch {
+    $vnet = New-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnetName -Location $Location -AddressPrefix "10.0.0.0/16"
+    Write-Host "Created vnet"
+}
+
+$subnetName = "exsubnet"
+try {
+    $subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnetName
+    Write-Host "Found subnet config"
+} catch {
+    $subnet = Add-AzVirtualNetworkSubnetConfig -Name $subnetName -VirtualNetwork $vnet -AddressPrefix "10.0.1.0/24"
+    $vnet | Set-AzVirtualNetwork | Out-Null
+    $vnet = Get-AzVirtualNetwork -ResourceGroupName $ResourceGroupName -Name $vnetName
+    $subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnetName
+    Write-Host "Created subnet config"
+}
+
+$storageName = "exbootstorage"
+try {
+    $storage = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $storageName
+    Write-Host "Found storage account"
+} catch {
+    $storage = New-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $storageName -Location $Location -SkuName "Standard_LRS"
+    Write-Host "Created storage account"
+}
+
+function Add-NetPerfVm {
+    param ($vmName)
+
+    try {
+        Get-AzVM -ResourceGroupName $ResourceGroupName -Name $vmName | Out-Null
+        Write-Host "`nFound $vmName already created. Skipping..."
+        return
+    } catch { }
+
+    Write-Host "`nCreating $vmName"
+
+    Write-Host "$vmName`: Creating IP address" # TODO - Remove need for public IP address
+    $publicIp = New-AzPublicIpAddress -ResourceGroupName $ResourceGroupName -Name "$vmName-PublicIP" -Location $Location -AllocationMethod "Static" -Force
+
+    Write-Host "$vmName`: Creating security group"
+    $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Name "$vmName-Nsg" -Location $Location -Force
+
+    Write-Host "$vmName`: Creating network interface card"
+    $nic = New-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name "$vmName-Nic" -Location $Location -SubnetId $subnet.Id -PublicIpAddressId $publicIp.Id -NetworkSecurityGroupId $nsg.Id -EnableAcceleratedNetworking -Force
+
+    Write-Host "$vmName`: Creating VM config"
+    if ($osType -eq "windows") {
+        $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $VMSize -SecurityType TrustedLaunch -EnableVtpm $false -EnableSecureBoot $false
+        $vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Windows -ComputerName $vmName -Credential $cred -ProvisionVMAgent -EnableAutoUpdate
+    } else {
+        $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $VMSize
+        $vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Linux -ComputerName $vmName -Credential $cred
+    }
+    $vmConfig = Set-AzVMSourceImage -VM $vmConfig -PublisherName $image.Split(":")[0] -Offer $image.Split(":")[1] -Skus $image.Split(":")[2] -Version $image.Split(":")[3]
+    $vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
+    $vmConfig = Set-AzVMBootDiagnostic -VM $vmConfig -Enable -ResourceGroupName $ResourceGroupName -StorageAccountName $storage.StorageAccountName
+
+    Write-Host "$vmName`: Creating VM"
+    New-AzVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $vmConfig | Out-Null
+
+    if ($osType -eq "windows") {
+        Write-Host "$vmName`: Enabling test signing"
+        Invoke-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $vmName -CommandId "RunPowerShellScript" -ScriptString "bcdedit /set testsigning on" | Out-Null
+
+        Write-Host "$vmName`: Restarting VM"
+        Restart-AzVM -ResourceGroupName $ResourceGroupName -Name $vmName
+    }
+}
+
+function Get-NetPerfVmPrivateIp {
+    param ($vmName)
+    $nic = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name "$vmName-Nic"
+    return $nic.IpConfigurations[0].PrivateIpAddress
+}
+
+$vmName1 = "ex-$osType-01" # TODO - Dynamically generate numbers
+$vmName2 = "ex-$osType-02"
+
+Add-NetPerfVm $vmName1
+Add-NetPerfVm $vmName2
+
+if ($GitHubToken) {
+
+    $ip1 = Get-NetPerfVmPrivateIp $vmName1
+    $ip2 = Get-NetPerfVmPrivateIp $vmName2
+
+    if ($osType -eq "windows") {
+        Write-Host "Configuring GitHub peer machine"
+        $scriptParams = @{
+            "Username" = $username
+            "Password" = $password
+            "PeerIP" = $ip1
+        }
+        Invoke-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $vmName2 -CommandId "RunPowerShellScript" -ScriptPath ".\setup-runner-windows.ps1" -Parameter $scriptParams | Out-Null
+
+        Write-Host "Configuring GitHub runner machine"
+        $scriptParams = @{
+            "Username" = $username
+            "Password" = $password
+            "PeerIP" = $ip2
+            "GitHubToken" = $GitHubToken
+            "RunnerLabels" = "os-$osType,azure-ex"
+        }
+        Invoke-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $vmName1 -CommandId "RunPowerShellScript" -ScriptPath ".\setup-runner-windows.ps1" -Parameter $scriptParams | Out-Null
+    } else {
+        Write-Host "Configuring Linux GitHub peer machine"
+        $scriptParams = @{
+            "username" = $username
+            "password" = $password
+            "peerip" = $ip1
+            "noreboot" = $true
+            "runnerlabels" = "os-$osType,azure-ex"
+        }
+        Invoke-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $vmName2 -CommandId "RunShellScript" -ScriptPath ".\setup-runner-linux.sh" -Parameter $scriptParams | Out-Null
+
+        Write-Host "Configuring Linux GitHub runner machine"
+        $scriptParams = @{
+            "username" = $username
+            "password" = $password
+            "peerip" = $ip2
+            "githubtoken" = $GitHubToken
+            "noreboot" = $true
+            "runnerlabels" = "os-$osType,azure-ex"
+        }
+        Invoke-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName $vmName1 -CommandId "RunShellScript" -ScriptPath ".\setup-runner-linux.sh" -Parameter $scriptParams | Out-Null
+    }
+}
---
+++ docs/onboard.md
@@ -0,0 +1,37 @@
+# Onboarding a New Project
+
+## Adding a New Test
+
+TODO
+
+## Triggering a netperf Run
+
+The [run-workflow.ps1](../run-workflow.ps1) script is used to trigger the appropriate test and wait for it to complete. The script leverages the [GitHub REST API](https://docs.github.com/en/rest) to trigger the workflow, wait for it to complete and then use the result to pass or fail the original caller.  For example:
+
+```PowerShell
+$url = "https://raw.githubusercontent.com/microsoft/netperf/main/run-workflow.ps1"
+iex "& { $(irm $url) } ${{ secrets.NET_PERF_TRIGGER }} quic ${{ github.sha }} ${{ github.ref }} ${{ github.event.pull_request.number }}"
+```
+
+To call this script, you must have a GitHub Personal Access Token (PAT) with the `public_repo` scope.  This token is stored as a secret in the repository and is passed to the script as the first argument.  The other arguments are the name of the test to run, the SHA of the commit to test, the branch or tag to test, and the pull request number if applicable.
+
+### Running from Another GitHub Repository
+
+Here are a couple explicit steps to onboard a GitHub repo to trigger netperf.
+
+1. Create a new PAT with the `public_repo` scope.
+2. Add the PAT as a secret your GitHub repository.
+3. Add a step to your workflow to trigger the netperf run. Example:
+
+```yaml
+steps:
+- name: Run NetPerf Workflow
+  shell: pwsh
+  run: |
+    $url = "https://raw.githubusercontent.com/microsoft/netperf/main/run-workflow.ps1"
+    if ('${{ secrets.NET_PERF_TRIGGER }}' -eq '') {
+        Write-Host "Not able to run because no secrets are available!"
+        return
+    }
+    iex "& { $(irm $url) } ${{ secrets.NET_PERF_TRIGGER }} quic ${{ github.sha }} ${{ github.ref }} ${{ github.event.pull_request.number }}"
+```
