2eaa78ac72291efe5000259ffb09efc8efc713e5
Jack He <jackhe@microsoft.com>
2024-03-29 13:38:47 -0700

    remove duplicate configs

--- setup-runner-linux.sh
+++ setup-runner-linux.sh
@@ -86,9 +86,9 @@ echo "================= Installing powershell 7. ================="
 sudo apt-get install -y wget apt-transport-https software-properties-common
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-sleep 5
 echo "================= Updating apt-get again. ================="
 sudo apt-get update
+sleep 5
 echo "================= Attempting to install powershell ================="
 sudo apt-get install powershell -y
 echo "Powershell 7 installed. Version:"
