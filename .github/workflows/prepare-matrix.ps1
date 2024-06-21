761a16e717236f5745a5d01768222646cc8f120c
Jack He (Github) <jackhe2345@gmail.com>
2024-06-21 15:20:34 -0700

    Add infrastructure to enable 2-machine network testing on 1ES Azure pools.  (#215)
    * add yaml code to kick off a pool job
    
    * synchronize
    
    * synchronize
    
    * this shit is so jank
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * install gh cli
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * use headers for secrets
    
    * stupid mistake fix
    
    * ping server ip
    
    * test net connectivity instead of ping
    
    * try directly using secnetperf
    
    * didn't know starting secnetperf server blocks execution
    
    * try disabling windows firewall
    
    * start server as a job
    
    * start server as a job
    
    * wip
    
    * add Receive-Job cmdlet
    
    * add dates to detail events
    
    * add a sanity check
    
    * cooking
    
    * Try link-local ipv6
    
    * Add quotes
    
    * Setup IP before download artifacts
    
    * Use first interface index
    
    * Try ULA address instead?
    
    * Revert "Try ULA address instead?"
    
    This reverts commit f5e633635c1b7bd41d4531b85b1ef6cffe98a248.
    
    * Revert "Use first interface index"
    
    This reverts commit 8ee81ce8be02994c2f8e1dc2cba57a97f0c32351.
    
    * Revert "Setup IP before download artifacts"
    
    This reverts commit 9c11fa4b648a97e41e0f309a170208e4de28a972.
    
    * Revert "Add quotes"
    
    This reverts commit e45fb98ebfb97abfc8e17e6d5674af3fe3d90cc6.
    
    * Revert "Try link-local ipv6"
    
    This reverts commit 39b81bebe304393babdf4deea623bdd338431be4.
    
    * use magic UDP packet
    
    * tell server to shut down after we upload ACK
    
    * i wanna see them greens. show me the money
    
    * use shared cache solution but integrated with quic
    
    * delete processed matrices
    
    * wip
    
    * try testing on linux
    
    * try again but with grep
    
    * openssl
    
    * get linux to work
    
    * fix syntax on linux
    
    * update permissions and LD_LIBRARY_PATH
    
    * use powershell
    
    * processes get killed between each step
    
    * netperf-peer
    
    * make it work for linux
    
    * get rid of the pesky print networking info
    
    * add LD_PATH stuff
    
    * wip
    
    * ubuntu 18 should be 20
    
    * add approved commands
    
    * add approved commands
    
    * GOAL: get a successful linux secnetperf run
    
    * lets get some data
    
    * post ip
    
    * less than not <
    
    * fix parse error
    
    * usepowershell instead of sh
    
    * POST not PUT
    
    * have some confidence
    
    * just get it working
    
    * custom yml
    
    * use pwsh
    
    * dumb paths
    
    * wip
    
    * add a chmod?
    
    * what does workspace look like?
    
    * wip
    
    * re-order instructions
    
    * syntax problem fix
    
    * load the lib
    
    * command not found lol
    
    * add lowlat to approved commands
    
    * lets improve
    
    * better logs
    
    * test it all together
    
    * fix syntax error
    
    * yml spaces >:(
    
    * whitespace
    
    * yaml do be kinda dumb
    
    * set requires POST method
    
    * update ref
    
    * download watermark json and add extra steps on windows for remote pwsh
    
    * should work this time
    
    * Update .github/workflows/prepare-matrix.yml
    
    Co-authored-by: Nick Banks <nibanks@microsoft.com>
    
    * refactor to prepare for merge
    
    ---------
    
    Co-authored-by: Nick Banks <nibanks@microsoft.com>

---
+++ .github/workflows/prepare-matrix.ps1
@@ -0,0 +1,49 @@
+
+param(
+    [Parameter(Mandatory=$false)]
+    [string]$MatrixFileName = "quic_matrix.json"
+)
+
+Set-StrictMode -Version "Latest"
+$PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+
+
+$MatrixJson = Get-Content -Path .\.github\workflows\$MatrixFileName | ConvertFrom-Json
+
+
+$AzureJson = @()
+$LabJson = @()
+
+foreach ($entry in $MatrixJson) {
+    if ($entry.env -match "azure") {
+        $Windows2022Pool = "netperf-aztestpool"
+        $Ubuntu2004Pool = "netperf-aztestpool-linux"
+        $client = $entry.PSObject.Copy()
+        $server = $entry.PSObject.Copy()
+        $env_str = "$($entry.os)-$($entry.arch)-$($entry.tls)-$($entry.io)"
+        if ($entry.os -match "windows-2022") {
+            $client | Add-Member -MemberType NoteProperty -Name "assigned_pool" -Value $Windows2022Pool
+            $server | Add-Member -MemberType NoteProperty -Name "assigned_pool" -Value $Windows2022Pool
+            $client | Add-Member -MemberType NoteProperty -Name "remote_powershell_supported" -Value 'TRUE'
+            $server | Add-Member -MemberType NoteProperty -Name "remote_powershell_supported" -Value 'TRUE'
+        }
+        if ($entry.os -match "ubuntu-20.04") {
+            $client | Add-Member -MemberType NoteProperty -Name "assigned_pool" -Value $Ubuntu2004Pool
+            $server | Add-Member -MemberType NoteProperty -Name "assigned_pool" -Value $Ubuntu2004Pool
+            $client | Add-Member -MemberType NoteProperty -Name "remote_powershell_supported" -Value 'FALSE'
+            $server | Add-Member -MemberType NoteProperty -Name "remote_powershell_supported" -Value 'FALSE'
+        }
+        $client | Add-Member -MemberType NoteProperty -Name "role" -Value "client"
+        $server | Add-Member -MemberType NoteProperty -Name "role" -Value "server"
+        $client | Add-Member -MemberType NoteProperty -Name "env_str" -Value $env_str
+        $server | Add-Member -MemberType NoteProperty -Name "env_str" -Value $env_str
+        $AzureJson += $client
+        $AzureJson += $server
+    } else {
+        $LabJson += $entry
+    }
+}
+
+# Save JSON to file
+$LabJson | ConvertTo-Json | Set-Content -Path .\.github\workflows\processed-matrix.json
+$AzureJson | ConvertTo-Json | Set-Content -Path .\.github\workflows\azure-matrix.json
