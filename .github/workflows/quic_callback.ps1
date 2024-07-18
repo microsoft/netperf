489692f27c13d89816fea633cc90bf22b360b127
Jack He <jackhe@microsoft.com>
2024-07-18 16:03:36 -0700

    fix callback params

--- .github/workflows/quic_callback.ps1
+++ .github/workflows/quic_callback.ps1
@@ -4,9 +4,9 @@ param (
 
 Write-Host "Executing command: $Command"
 
-if ($Command -eq "START_SERVER_LOWLAT") {
+if ($Command -eq "/home/secnetperf/_work/quic/artifacts/bin/linux/x64_Release_openssl/secnetperf -exec:lowlat -io:epoll -stats:1") {
     ./artifacts/bin/linux/x64_Release_openssl/secnetperf -exec:lowlat -io:epoll -stats:1 | Out-Null
-} elseif ($Command -eq "START_SERVER_MAXTPUT") {
+} elseif ($Command -eq "/home/secnetperf/_work/quic/artifacts/bin/linux/x64_Release_openssl/secnetperf -exec:maxtput -io:epoll -stats:1") {
     ./artifacts/bin/linux/x64_Release_openssl/secnetperf -exec:maxtput -io:epoll -stats:1 | Out-Null
 } else {
     throw "Invalid command: $Command"
