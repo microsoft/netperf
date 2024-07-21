df5851a08842e1ec790dacb93506735df15d0011
Jack He <jackhe@microsoft.com>
2024-07-21 13:02:53 -0700

    wip

---
+++ 1es-machine-manager.ps1
@@ -0,0 +1,22 @@
+param (
+    [string]$Action,
+    [string]$GithubContextInput = "",
+)
+
+if ($Action -eq "Deserialize_matrix") {
+    $matrix = ConvertFrom-Json $GithubContextInput
+    $remote_powershell_supported = $matrix.remote_powershell_supported
+    $role = $matrix.role
+    $env_str = $matrix.env_str
+    echo "remote_powershell_supported=$remote_powershell_supported" >> $env:GITHUB_ENV
+    echo "role=$role" >> $env:GITHUB_ENV
+    echo "env_str=$env_str" >> $env:GITHUB_ENV
+}
+
+if ($Action -eq "Disable_Windows_Defender") {
+    # Disable Windows defender / firewall.
+    Write-Host "Disabling Windows Defender / Firewall."
+    netsh.exe advfirewall set allprofiles state off
+    Set-MpPreference -EnableNetworkProtection Disabled
+    Set-MpPreference -DisableDatagramProcessing $True
+}
