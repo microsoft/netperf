3f3831eb9a372d42368cd1f7bf8b71ec1f8f35d0
Jack He <jackhe2345@gmail.com>
2024-08-06 14:17:18 -0700

    Implement support for Exp. Boost + Refactor secnetperf.ps1 for better usability. (#261)
    * start process of refactoring to onboard ebpf
    
    * try with using a composite workflow
    
    * add boosted pool testing code
    
    * what are composite actions
    
    * fix dumb syntax error
    
    * define actions in their own subfolder
    
    * the rules are murky here
    
    * add a sanity check
    
    * improve sanity check
    
    * improve sanity check
    
    * yaml
    
    * yaml is confusing
    
    * pass secret as a param
    
    * wait until you download everything first
    
    * lowercase runner.os first step
    
    * make it so netperf is a subdirectory in the main project
    
    * use the updated api
    
    * temp change for testing
    
    * change api endpoint
    
    * try and see if the boost pool will work without a managed image
    
    * try installing powershell in the job itself
    
    * netp boosted windows pool?
    
    * deref deleted branch
    
    * point to the same api
    
    * add common lib
    
    * add instructions for how to onboard a new test / project for Azure
    
    * add netperf-lib code
    
    * add library scripts
    
    * fix syntax issue
    
    * try passing in matrix obj directly. hopefully it automatically serializes it
    
    * install pwsh if need be
    
    * didn't serialize. Defaulted to 'Object'
    
    * make it into a string
    
    * add quotes on stop job as well
    
    * syntax issue fix
    
    * get rid of install powershell step from bad fx sku vm size
    
    * perhaps you need to add a type?
    
    * install pwsh if needed
    
    * each step requires a 'shell' block
    
    * single quotes instead of double quotes
    
    * add inherited params
    
    * remove timeout minutes in composite action
    
    * silly mistake
    
    * update docs
    
    * think through how to add tests and onboard new projects
    
    * wip
    
    * add a prepare-1es-machine composite action
    
    * add wrapper scripts
    
    * fix params
    
    * bash not in windows
    
    * fix callback params
    
    * add more commands to quic_callback
    
    * maybe this will work?
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * does adding pwsh to path work?
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * xdp wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * wsk wip
    
    * ws2022, ws2025, linux all green
    
    * add 2025 support
    
    * default to f series cause exp. boost is tweaking
    
    * improve pwsh logic
    
    * test 2025
    
    * Update quic_matrix.json
    
    * test everything, make sure it all runs
    
    * unix xdp is broken
    
    * test azure stuff
    
    * bash ain't powershell
    
    * GITHUB_ENV properly set
    
    * no more watchdog on callback
    
    * fix error message print out
    
    * is this a capacity issue?
    
    * need to copy kernel stuff to usernmode path
    
    * is this a wsk issue? Or something else?
    
    * is this a windows 2022 image problem?
    
    * remember to disable windows defender on windows
    
    * run the full gauntlet
    
    * commit full matrix
    
    * ref should point to main

---
+++ netperf-lib.psm1
@@ -0,0 +1,85 @@
+
+function ValidateInheritedParams {
+    Write-Host "Inherited run id: $env:netperf_run_id"
+    Write-Host "Inherited role: $env:netperf_role"
+    Write-Host "Inherited remote_powershell_supported: $env:netperf_remote_powershell_supported"
+    Write-Host "Inherited api url: $env:netperf_api_url"
+    $headers = @{
+        'secret' = $env:netperf_syncer_secret
+    }
+    Invoke-WebRequest -Uri "$env:netperf_api_url/hello" -Headers $headers
+}
+
+function NetperfSendCommand {
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Command
+    )
+    Write-Host "Sending command: $Command"
+    # Should send a command to the shared cache and wait for the server process to execute said command before exiting.
+    $headers = @{
+        "secret" = "$env:netperf_syncer_secret"
+    }
+    $url = "$env:netperf_api_url"
+    $RunId = "$env:netperf_run_id"
+    try {
+        $Response = Invoke-WebRequest -Uri "$url/getkeyvalue?key=$RunId" -Headers $headers -UseBasicParsing
+    } catch {
+        Write-Host "Unable to fetch state. Creating a new one now."
+        $state = [pscustomobject]@{
+            value=[pscustomobject]@{
+            "SeqNum" = 0
+            "Commands" = @($Command)
+        }}
+        $StateJson = $state | ConvertTo-Json
+        $Response = Invoke-WebRequest -Uri "$url/setkeyvalue?key=$RunId" -Headers $headers -Method Post -Body $StateJson -ContentType "application/json" -UseBasicParsing
+        if ($Response.StatusCode -ne 200) {
+            throw "Failed to set the key value!"
+        }
+        return
+    }
+    $CurrState = $Response.Content | ConvertFrom-Json
+    $CurrState.Commands += $Command
+    $CurrState = [pscustomobject]@{
+        value=$CurrState
+    }
+    $StateJson = $CurrState | ConvertTo-Json
+    $Response = Invoke-WebRequest -Uri "$url/setkeyvalue?key=$RunId" -Headers $headers -Method Post -Body $StateJson -ContentType "application/json" -UseBasicParsing
+    if ($Response.StatusCode -ne 200) {
+        throw "Failed to set the key value!"
+    }
+}
+
+function NetperfWaitServerFinishExecution {
+    param (
+        [Parameter(Mandatory = $false)]
+        [int]$MaxAttempts = 30,
+        [Parameter(Mandatory = $false)]
+        [int]$WaitPerAttempt = 8,
+        [Parameter(Mandatory = $false)]
+        [scriptblock]$UnblockRoutine = {}
+    )
+
+    for ($i = 0; $i -lt $maxattempts; $i++) {
+        $UnblockRoutine.Invoke()
+        Write-Host "Waiting for server to finish execution... Attempt $i"
+        Start-Sleep -Seconds $WaitPerAttempt
+        $headers = @{
+            "secret" = "$env:netperf_syncer_secret"
+        }
+        $url = "$env:netperf_api_url"
+        $RunId = "$env:netperf_run_id"
+        $Response = Invoke-WebRequest -Uri "$url/getkeyvalue?key=$RunId" -Headers $headers -UseBasicParsing
+        if (!($Response.StatusCode -eq 200)) {
+            throw "Remote Cache State Not Set!"
+        }
+        $CurrState = $Response.Content | ConvertFrom-Json
+        if ($CurrState.SeqNum -eq $CurrState.Commands.Count) {
+            return
+        } else {
+            Write-Host "Server not done yet. Seq num: $($CurrState.SeqNum), Commands count: $($CurrState.Commands.Count)"
+        }
+    }
+
+    throw "Server did not finish execution in time! Tried $maxattempts times with $WaitPerAttempt seconds interval."
+}
---
+++ set-netperf-context.ps1
@@ -0,0 +1,41 @@
+param (
+    [string]$Matrix,
+    [string]$GithubRunId,
+    [string]$SyncerSecret
+)
+
+$DeserializedMatrix = ConvertFrom-Json $Matrix
+
+$Boolean = @('TRUE', 'FALSE')
+$ValidRoles = @('client', 'server')
+
+if (!($Boolean -contains $DeserializedMatrix.remote_powershell_supported)) {
+    throw "Invalid remote_powershell_supported value: $($DeserializedMatrix.remote_powershell_supported). Did you forget to run prepare-matrix.ps1?"
+    exit 1
+}
+
+if (!($ValidRoles -contains $DeserializedMatrix.role)) {
+    throw "Invalid role value: $($DeserializedMatrix.role). Did you forget to run prepare-matrix.ps1?"
+    exit 1
+}
+
+$envstr = $DeserializedMatrix.env_str
+
+if ($envstr.Length -lt 1 -or $GithubRunId.Length -lt 1) {
+    throw "Invalid env_str inputs and / or run_id"
+    exit 1
+}
+
+Write-Host "Current environment string: $envstr"
+if ($DeserializedMatrix.remote_powershell_supported -eq 'TRUE') {
+    Write-Host "Remote powershell is supported."
+    $env:netperf_remote_powershell_supported = $true
+} else {
+    Write-Host "Remote powershell is not supported."
+    $env:netperf_remote_powershell_supported = $false
+}
+
+$env:netperf_role = $DeserializedMatrix.role
+$env:netperf_run_id = "$GithubRunId-$envstr-state"
+$env:netperf_api_url = "https://netperfapi.azurewebsites.net"
+$env:netperf_syncer_secret = $SyncerSecret
