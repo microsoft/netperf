db26977b85ab62e7192d54168e55d6cefe5d50a9
Jack He <jackhe@microsoft.com>
2025-08-15 12:16:27 -0700

    copy everything

--- netperf-lib.psm1
+++ netperf-lib.psm1
@@ -180,7 +180,7 @@ function Copy-RepoToPeer {
             }
             New-Item -ItemType Directory -Path $Using:RemoteDir -Force | Out-Null
         }
-        Copy-Item -Path . -Destination "$RemoteDir" -Recurse -Force -ToSession $Session
+        Copy-Item -ToSession $Session -Path ./*, ./.* -Destination "$RemoteDir" -Recurse -Force
     } else {
         Write-Host "Not using remote powershell, assuming peer has checked out the repo."
     }
